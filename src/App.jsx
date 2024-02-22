106b7e97a0d7eebec9b517e2c6c3261fae58223d
DoCongThanhPhuong <bido23082003@gmail.com>
2024-02-22 22:04:47 +0700

    Config MUI Theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
 import Button from '@mui/material/Button'
+import Typography from '@mui/material/Typography'
 
 function App() {
   return (
     <>
       <div>DoCongThanhPhuong</div>
+
+      <Typography variant='body2' color='text.secondary'>Test Typography</Typography>
+
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
       <Button variant="outlined">Outlined</Button>
