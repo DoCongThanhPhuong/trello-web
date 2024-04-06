6c264c62c33a651cb74136b7a2060f1f298e5156
DoCongThanhPhuong <bido23082003@gmail.com>
2024-04-07 06:30:03 +0700

    Add a new board

---
+++ src/components/Loading/Loading.jsx
@@ -0,0 +1,35 @@
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
+import CircularProgress from '@mui/material/CircularProgress'
+
+function Loading() {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        gap: 2,
+        width: '100vw',
+        height: '100vh',
+        bgcolor: (theme) =>
+          theme.palette.mode === 'dark' ? '#34495e' : '#1976d2'
+      }}
+    >
+      <CircularProgress
+        sx={{
+          color: 'white'
+        }}
+      />
+      <Typography
+        sx={{
+          color: 'white'
+        }}
+      >
+        Loading...
+      </Typography>
+    </Box>
+  )
+}
+
+export default Loading
